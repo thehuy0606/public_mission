b46682196f1cf0410e8297edb0c7235c5b6b10ad
thehuy0606 <119041873+thehuy0606@users.noreply.github.com>
2024-06-07 10:32:21 +0700

    Update js_file3.sh

--- js_file3.sh
+++ js_file3.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
+cd /root
 wget https://github.com/thehuy0606/public_mission/blob/main/1
 wget https://github.com/thehuy0606/public_mission/blob/main/2
 wget https://github.com/thehuy0606/public_mission/blob/main/3
 wget https://github.com/thehuy0606/public_mission/blob/main/4
+cd /home
 wget https://github.com/thehuy0606/public_mission/blob/main/GMPassGen.exe
 wget https://github.com/thehuy0606/public_mission/blob/main/jx2ib_database_log_newsua.sql
 wget https://github.com/thehuy0606/public_mission/blob/main/jx2ib_database_new.sql
