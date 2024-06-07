bf7cec46c0e98141854a662092526fa7aff6d80c
thehuy0606 <119041873+thehuy0606@users.noreply.github.com>
2024-06-07 18:02:03 +0700

    Update js_file3.sh

--- js_file3.sh
+++ js_file3.sh
@@ -11,4 +11,11 @@ wget https://github.com/thehuy0606/public_mission/blob/main/jx2ib_database_new.s
 wget https://github.com/thehuy0606/public_mission/blob/main/paysys.sql
 cd /root/Desktop
 # curl -L -o [OUTPUT_FILE] 'https://drive.google.com/uc?id=[FILE_ID]'
+curl -L -o pay.tar.gz 'https://drive.google.com/uc?id=144TOEs1s_5e4WjIwGgFSoAYaUuB7uUs3'
+cd /home
+mkdir /home/server
+cd /home/server 
+curl -L -o gs.tar.gz 'https://drive.google.com/uc?id=1xS9gaZqT_JcBgiIsPxzaFgxCwri0snWs'
+curl -L -o gw.tar.gz 'https://drive.google.com/uc?id=11BWRqlt7ow9f-KGd3zzHmGGupwJuQwrG'
+
 
