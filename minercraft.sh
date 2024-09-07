22eb502aa9725323e22a6b7fd22935a6517a7e32
thehuy0606 <119041873+thehuy0606@users.noreply.github.com>
2024-09-07 23:14:35 +0700

    Update minercraft.sh

--- minercraft.sh
+++ minercraft.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 yum -y update && yum -y upgrade
 yum install wget vim screen bzip2 nano net-tools cronie -y 
-yum install openjdk-21-jdk
+yum install openjdk-17-jdk
 mkdir –p /Minecraft
 cd /Minecraft
 wget -O minecraft_server.jar https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar
